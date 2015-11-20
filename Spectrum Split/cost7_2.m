3a992b014a1dae55a4929482d0c1183da0bcc2ba
Ashish Tibrewal <at12060@my.bristol.ac.uk>
2015-11-20 13:58:27 +0000

    Added some comments to a few source files. Need to the the same for the others.
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   Spectrum Split/cost7_2.m
    	modified:   Spectrum Split/prioritymap7core_start2.m
    	modified:   Spectrum Split/prioritymap7core_uni.m
    	modified:   Spectrum Split/res20spineleaf.m
    	modified:   Spectrum Split/res20star.m
    	modified:   Spectrum Split/res6nodes.m
    
     Changes not staged for commit:
    	modified:   Algorithms comparison/cost7_2.m
    	modified:   Algorithms comparison/prioritymap7core_start2.m
    	modified:   Algorithms comparison/prioritymap7core_uni.m
    	modified:   Algorithms comparison/res20spineleaf.m
    	modified:   Algorithms comparison/res20star.m
    	modified:   Algorithms comparison/res6nodes.m

--- Spectrum Split/cost7_2.m
+++ Spectrum Split/cost7_2.m
@@ -1,86 +1,95 @@
-function corecost=cost7_2(corecost,corenum)
+% Function to generate core cost for a 7 core fibre
+% Function parameters:
+% corecost - Matrix containing the core cost values
+% corenum - Value containing the current core number
+% Function return values:
+% corecost - Updated core cost value for a particular core
 
-corecost(corenum)=inf;
+function corecost = cost7_2(corecost,corenum)
 
-if corenum==1
-    for i=2:1:7
-        corecost(i)=corecost(i)+1;
-    end
-end
+  corecost(corenum)=inf;
 
-if corenum==2
-    corecost(1)=corecost(1)+1;
-    corecost(3)=corecost(3)+1;
-    corecost(7)=corecost(7)+1;
-end
+  if corenum==1
+      for i=2:1:7
+          corecost(i)=corecost(i)+1;
+      end
+  end
 
-if corenum==3
-    corecost(1)=corecost(1)+1;
-    corecost(2)=corecost(2)+1;
-    corecost(4)=corecost(4)+1;
-end
+  if corenum==2
+      corecost(1)=corecost(1)+1;
+      corecost(3)=corecost(3)+1;
+      corecost(7)=corecost(7)+1;
+  end
 
-if corenum==4
-    corecost(1)=corecost(1)+1;
-    corecost(3)=corecost(3)+1;
-    corecost(5)=corecost(5)+1;
-end
+  if corenum==3
+      corecost(1)=corecost(1)+1;
+      corecost(2)=corecost(2)+1;
+      corecost(4)=corecost(4)+1;
+  end
 
-if corenum==5
-    corecost(1)=corecost(1)+1;
-    corecost(4)=corecost(4)+1;
-    corecost(6)=corecost(6)+1;
-end
+  if corenum==4
+      corecost(1)=corecost(1)+1;
+      corecost(3)=corecost(3)+1;
+      corecost(5)=corecost(5)+1;
+  end
 
-if corenum==6
-    corecost(1)=corecost(1)+1;
-    corecost(5)=corecost(5)+1;
-    corecost(7)=corecost(7)+1;
-end
+  if corenum==5
+      corecost(1)=corecost(1)+1;
+      corecost(4)=corecost(4)+1;
+      corecost(6)=corecost(6)+1;
+  end
 
-if corenum==7
-    corecost(1)=corecost(1)+1;
-    corecost(2)=corecost(2)+1;
-    corecost(6)=corecost(6)+1;
-end
-if corenum==8
-    for i=9:1:14
-        corecost(i)=corecost(i)+1;
-    end
-end
+  if corenum==6
+      corecost(1)=corecost(1)+1;
+      corecost(5)=corecost(5)+1;
+      corecost(7)=corecost(7)+1;
+  end
 
-if corenum==9
-    corecost(10)=corecost(10)+1;
-    corecost(8)=corecost(8)+1;
-    corecost(14)=corecost(14)+1;
-end
+  if corenum==7
+      corecost(1)=corecost(1)+1;
+      corecost(2)=corecost(2)+1;
+      corecost(6)=corecost(6)+1;
+  end
+  if corenum==8
+      for i=9:1:14
+          corecost(i)=corecost(i)+1;
+      end
+  end
 
-if corenum==10
-    corecost(8)=corecost(8)+1;
-    corecost(9)=corecost(9)+1;
-    corecost(11)=corecost(11)+1;
-end
+  if corenum==9
+      corecost(10)=corecost(10)+1;
+      corecost(8)=corecost(8)+1;
+      corecost(14)=corecost(14)+1;
+  end
 
-if corenum==11
-    corecost(8)=corecost(8)+1;
-    corecost(10)=corecost(10)+1;
-    corecost(12)=corecost(12)+1;
-end
+  if corenum==10
+      corecost(8)=corecost(8)+1;
+      corecost(9)=corecost(9)+1;
+      corecost(11)=corecost(11)+1;
+  end
 
-if corenum==12
-    corecost(8)=corecost(8)+1;
-    corecost(11)=corecost(11)+1;
-    corecost(13)=corecost(13)+1;
-end
+  if corenum==11
+      corecost(8)=corecost(8)+1;
+      corecost(10)=corecost(10)+1;
+      corecost(12)=corecost(12)+1;
+  end
 
-if corenum==13
-    corecost(14)=corecost(14)+1;
-    corecost(12)=corecost(12)+1;
-    corecost(8)=corecost(8)+1;
-end
+  if corenum==12
+      corecost(8)=corecost(8)+1;
+      corecost(11)=corecost(11)+1;
+      corecost(13)=corecost(13)+1;
+  end
 
-if corenum==14
-    corecost(8)=corecost(8)+1;
-    corecost(9)=corecost(9)+1;
-    corecost(13)=corecost(13)+1;
+  if corenum==13
+      corecost(14)=corecost(14)+1;
+      corecost(12)=corecost(12)+1;
+      corecost(8)=corecost(8)+1;
+  end
+
+  if corenum==14
+      corecost(8)=corecost(8)+1;
+      corecost(9)=corecost(9)+1;
+      corecost(13)=corecost(13)+1;
+  end
+  
 end
