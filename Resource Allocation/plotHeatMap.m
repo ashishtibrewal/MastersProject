56e29378750ff9802c9fffac0c0672bf8886c2d3
Ashish Tibrewal <at12060@my.bristol.ac.uk>
2016-03-07 02:50:51 +0000

    Improved resource location and heat map plots.
     On branch test
     Your branch is up-to-date with 'origin/test'.
    
     Changes to be committed:
    	modified:   Resource Allocation/config/config.yaml
    	modified:   Resource Allocation/plotHeatMap.m

--- Resource Allocation/plotHeatMap.m
+++ Resource Allocation/plotHeatMap.m
@@ -13,14 +13,31 @@ function plotHeatMap(dataCenterConfig, dataCenterMap, updateType)
   nTORs = nTOR * nRacks;
   nTOBs = nTOB * nBlades * nRacks;
   nNodes = nSlots * nBlades * nRacks;
+  nNodesPerRack = nSlots * nBlades;
   
   startNode = (nTORs + nTOBs);
   endNode = startNode + nNodes;
   
+
+  % Find number of subfigures required
+  nFigsFactors = [];
+  for i = 1:(nRacks * 2)
+    if (mod((nRacks * 2),i) == 0)
+      nFigsFactors = [nFigsFactors,i];
+    end
+  end
+
+  nFigsFactorsSize = size(nFigsFactors,2);
+  if (mod(nFigsFactorsSize,2) == 0)
+    nSubFigs = [nFigsFactors(nFigsFactorsSize/2), nFigsFactors((nFigsFactorsSize/2) + 1)];
+  else
+    nSubFigs = [nFigsFactors(ceil(nFigsFactorsSize/2)), nFigsFactors(ceil(nFigsFactorsSize/2))];
+  end
+
   % Find factors (multiples) of nNodes to be able to create a scalable heatmap
   nNodesFactors = [];
-  for i = 1:nNodes
-    if (mod(nNodes,i) == 0)
+  for i = 1:nNodesPerRack
+    if (mod(nNodesPerRack,i) == 0)
       nNodesFactors = [nNodesFactors,i];
     end
   end
@@ -32,6 +49,8 @@ function plotHeatMap(dataCenterConfig, dataCenterMap, updateType)
     hmSize = [nNodesFactors(ceil(nNodesFactorsSize/2)), nNodesFactors(ceil(nNodesFactorsSize/2))];
   end
 
+  hmSize = [nBlades, nSlots];
+
   scaleFactor = 8;
 
   rmap = zeros([hmSize,3]);
@@ -39,56 +58,173 @@ function plotHeatMap(dataCenterConfig, dataCenterMap, updateType)
   
   hmap = zeros([hmSize,3]);
   hmapScaled = zeros([(hmSize * scaleFactor),3]);
+
+%   x = linspace(0,nSlots,(nSlots + 1));
+%   y = linspace(0,nBlades,(nBlades + 1));
+% 
+%   % Horizontal grid 
+%   for k = 1:length(y)
+%     line([x(1) x(end)], [y(k) y(k)]);
+%   end
+% 
+%   % Vertical grid
+%   for k = 1:length(x)
+%     line([x(k) x(k)], [y(1) y(end)]);
+%   end
+% 
+%   axis square
   
   switch (updateType)
     case 'locationMap'
-      for r = 1:hmSize(1)
-        for c = 1:hmSize(2)
-          nodeType = dataCenterMap.completeResourceMap(startNode + ((r - 1) * hmSize(2)) + c);
-          if (strcmp(nodeType, 'CPU') == 1)
-              rmap(r,c,:) = [0,0,0];
-          elseif (strcmp(nodeType, 'MEM') == 1)
-              rmap(r,c,:) = [1,1,1];
-          elseif (strcmp(nodeType, 'STO') == 1)
-              rmap(r,c,:) = [0.5,0.5,0.5];
-          end
-        end
-      end
-
-      for r = 1:hmSize(1)
-        for c = 1:hmSize(2)
-          rmapScaled((((r - 1) * scaleFactor) + 1):(r * scaleFactor),(((c - 1) * scaleFactor) + 1):(c * scaleFactor),1) = rmap(r,c,1);
-          rmapScaled((((r - 1) * scaleFactor) + 1):(r * scaleFactor),(((c - 1) * scaleFactor) + 1):(c * scaleFactor),2) = rmap(r,c,2);
-          rmapScaled((((r - 1) * scaleFactor) + 1):(r * scaleFactor),(((c - 1) * scaleFactor) + 1):(c * scaleFactor),3) = rmap(r,c,3);
-        end
-      end
-
       % Open new figure
       figure ('Name', 'Data Center Heatmap', 'NumberTitle', 'off', 'Position', [150, 50, 1000, 700]);
-      subplot(1,2,1);
-      imshow(rmapScaled);
-      title('Resource type - CPUs = Black, MEMs = While, STOs = Grey');
+      figNo = 1;
+      for i = 1:nRacks
+        startNode = (nTORs + nTOBs) + ((i - 1) * (nSlots * nBlades));
+        for r = 1:hmSize(1)
+          for c = 1:hmSize(2)
+            nodeType = dataCenterMap.completeResourceMap(startNode + ((r - 1) * hmSize(2)) + c);
+            if (strcmp(nodeType, 'CPU') == 1)
+                rmap(r,c,:) = [0,0,0];
+            elseif (strcmp(nodeType, 'MEM') == 1)
+                rmap(r,c,:) = [1,1,1];
+            elseif (strcmp(nodeType, 'STO') == 1)
+                rmap(r,c,:) = [0.5,0.5,0.5];
+            end
+          end
+        end
+
+        for r = 1:hmSize(1)
+          for c = 1:hmSize(2)
+            rmapScaled((((r - 1) * scaleFactor) + 1):(r * scaleFactor),(((c - 1) * scaleFactor) + 1):(c * scaleFactor),1) = rmap(r,c,1);
+            rmapScaled((((r - 1) * scaleFactor) + 1):(r * scaleFactor),(((c - 1) * scaleFactor) + 1):(c * scaleFactor),2) = rmap(r,c,2);
+            rmapScaled((((r - 1) * scaleFactor) + 1):(r * scaleFactor),(((c - 1) * scaleFactor) + 1):(c * scaleFactor),3) = rmap(r,c,3);
+          end
+        end
+
+        subtightplot(nSubFigs(1),nSubFigs(2),figNo,[0.03,0.01],[0.01,0.03],[0.01,0.01]);
+
+        %rectangle('Position', [0, 0, (nSlots * 2), (nBlades * 2)]);
+        for r = 1:hmSize(1)
+          for c = 1:hmSize(2)
+            nodeType = dataCenterMap.completeResourceMap(startNode + ((r - 1) * hmSize(2)) + c);
+            if (strcmp(nodeType, 'CPU') == 1)
+                colorRGB = [0,0,0];
+                rectangle('Position', [(c * 2), (r * 2), 2, 2], 'FaceColor', colorRGB, 'EdgeColor', [1,1,1]);
+            elseif (strcmp(nodeType, 'MEM') == 1)
+                colorRGB = [1,1,1];
+                rectangle('Position', [(c * 2), (r * 2), 2, 2], 'FaceColor', colorRGB);
+            elseif (strcmp(nodeType, 'STO') == 1)
+                colorRGB = [0.5,0.5,0.5];
+                rectangle('Position', [(c * 2), (r * 2), 2, 2], 'FaceColor', colorRGB);
+            end
+          end
+        end
+        axis off;
+        str = sprintf('Rack %d Setup', i);
+        title(str);
+
+%         x = linspace(0,nSlots,(nSlots + 1));
+%         y = linspace(0,nBlades,(nBlades + 1));
+% 
+%         % Horizontal grid 
+%         for k = 1:length(y)
+%           line([x(1) x(end)], [y(k) y(k)]);
+%         end
+% 
+%         % Vertical grid
+%         for k = 1:length(x)
+%           line([x(k) x(k)], [y(1) y(end)]);
+%         end
+%         
+%         axis off;
+        
+%         subtightplot(nSubFigs(1),nSubFigs(2),figNo,[0.01,0],0,0);
+%         imshow(rmapScaled);
+%         str = sprintf('Rack %d S', i);
+%         title(str);
+%         if (mod(i,nSubFigs(2)) == 0)
+%           figNo = figNo + (nSubFigs(2) + 1);
+%         else
+%           figNo = figNo + 1;
+%         end
+
+        if (mod(i,nSubFigs(2)) == 0)
+          figNo = figNo + (nSubFigs(2) + 1);
+        else
+          figNo = figNo + 1;
+        end
+      end
+
+      %title('Resource type - CPUs = Black, MEMs = While, STOs = Grey');
       
     case 'heatMap'
-      for r = 1:hmSize(1)
-        for c = 1:hmSize(2)
-          nodeVal = dataCenterMap.completeUnitAvailableMap(startNode + ((r - 1) * hmSize(2)) + c);
-          avalibilityRatio = nodeVal/nUnits;
-          hmap(r,c,:) = [(1 - avalibilityRatio), avalibilityRatio, 0];
+      figNo = nSubFigs + 1;
+      for i = 1:nRacks
+        startNode = (nTORs + nTOBs) + ((i - 1) * (nSlots * nBlades));
+        for r = 1:hmSize(1)
+          for c = 1:hmSize(2)
+            nodeVal = dataCenterMap.completeUnitAvailableMap(startNode + ((r - 1) * hmSize(2)) + c);
+            avalibilityRatio = nodeVal/nUnits;
+            hmap(r,c,:) = [(1 - avalibilityRatio), avalibilityRatio, 0];
+          end
+        end
+
+        for r = 1:hmSize(1)
+          for c = 1:hmSize(2)
+            hmapScaled((((r - 1) * scaleFactor) + 1):(r * scaleFactor),(((c - 1) * scaleFactor) + 1):(c * scaleFactor),1) = hmap(r,c,1);
+            hmapScaled((((r - 1) * scaleFactor) + 1):(r * scaleFactor),(((c - 1) * scaleFactor) + 1):(c * scaleFactor),2) = hmap(r,c,2);
+            hmapScaled((((r - 1) * scaleFactor) + 1):(r * scaleFactor),(((c - 1) * scaleFactor) + 1):(c * scaleFactor),3) = hmap(r,c,3);
+          end
+        end
+
+        subtightplot(nSubFigs(1),nSubFigs(2),figNo,[0.03,0.01],[0.01,0.03],[0.01,0.01]);
+
+        for r = 1:hmSize(1)
+          for c = 1:hmSize(2)
+            nodeVal = dataCenterMap.completeUnitAvailableMap(startNode + ((r - 1) * hmSize(2)) + c);
+            avalibilityRatio = nodeVal/nUnits;
+            colorRGB = [(1 - avalibilityRatio), avalibilityRatio, 0];
+            rectangle('Position', [(c * 2), (r * 2), 2, 2], 'FaceColor', colorRGB);
+          end
+        end
+        axis off;
+        str = sprintf('Rack %d Utilisation', i);
+        title(str);
+
+%         x = linspace(0,nSlots,(nSlots + 1));
+%         y = linspace(0,nBlades,(nBlades + 1));
+% 
+%         % Horizontal grid 
+%         for k = 1:length(y)
+%           line([x(1) x(end)], [y(k) y(k)]);
+%         end
+% 
+%         % Vertical grid
+%         for k = 1:length(x)
+%           line([x(k) x(k)], [y(1) y(end)]);
+%         end
+%       
+%         axis off;
+
+%         subtightplot(nSubFigs(1),nSubFigs(2),figNo,[0.01,0],0,0);
+%         imshow(rmapScaled);
+%         str = sprintf('Rack %d U', i);
+%         title(str);
+%         if (mod(i,nSubFigs(2)) == 0)
+%           figNo = figNo + (nSubFigs(2) + 1);
+%         else
+%           figNo = figNo + 1;
+%         end
+
+        if (mod(i,nSubFigs(2)) == 0)
+          figNo = figNo + (nSubFigs(2) + 1);
+        else
+          figNo = figNo + 1;
         end
       end
 
-      for r = 1:hmSize(1)
-        for c = 1:hmSize(2)
-          hmapScaled((((r - 1) * scaleFactor) + 1):(r * scaleFactor),(((c - 1) * scaleFactor) + 1):(c * scaleFactor),1) = hmap(r,c,1);
-          hmapScaled((((r - 1) * scaleFactor) + 1):(r * scaleFactor),(((c - 1) * scaleFactor) + 1):(c * scaleFactor),2) = hmap(r,c,2);
-          hmapScaled((((r - 1) * scaleFactor) + 1):(r * scaleFactor),(((c - 1) * scaleFactor) + 1):(c * scaleFactor),3) = hmap(r,c,3);
-        end
-      end
-
-      subplot(1,2,2);
-      imshow(hmapScaled);
-      title('Resource Utilization - Green = Max free, Yellow = Min free, Red = None free');
+      %title('Resource Utilization - Green = Max free, Yellow = Min free, Red = None free');
       
       %blocked = find(cell2mat(requestDB(1:t,9)) == 0);
       %nBlocked = [nBlocked,size(blocked,1)];
