cb48bb6a68d07412940bd44320f800c21d08ab14
Ashish Tibrewal <at12060@my.bristol.ac.uk>
2016-04-19 18:31:35 +0100

    Removed redundant code.
     On branch localityAware
     Your branch is up-to-date with 'origin/localityAware'.
    
     Changes to be committed:
    	modified:   resourceAllocation.m

--- Resource Allocation/resourceAllocation.m
+++ Resource Allocation/resourceAllocation.m
@@ -249,7 +249,6 @@ function [dataCenterMap, ITallocationResult, NETallocationResult, ITresourceNode
               else    % Actual failure cause is network resources
                 NETresourceUnavailable = 1;
                 NETfailureCause = 'BAN';
-                ITfailureCause = 'NONE';    % Reset the value returned by BFS since the actual failure cause was network resources and not IT resources
                 NETresourceUnavailableBFS = 1;
                 break;    % Break out of inner scan loop since not enough NET resources are available; any further tries would fail
               end
@@ -328,7 +327,6 @@ function [dataCenterMap, ITallocationResult, NETallocationResult, ITresourceNode
               else    % Actual failure cause is network resources
                 NETresourceUnavailable = 1;
                 NETfailureCause = 'BAN';
-                ITfailureCause = 'NONE';    % Reset the value returned by BFS since the actual failure cause was network resources and not IT resources
                 NETresourceUnavailableBFS = 1;
                 break;    % Break out of inner scan loop since not enough NET resources are available; any further tries would fail
               end
@@ -407,7 +405,6 @@ function [dataCenterMap, ITallocationResult, NETallocationResult, ITresourceNode
               else    % Actual failure cause is network resources
                 NETresourceUnavailable = 1;
                 NETfailureCause = 'BAN';
-                ITfailureCause = 'NONE';    % Reset the value returned by BFS since the actual failure cause was network resources and not IT resources
                 NETresourceUnavailableBFS = 1;
                 break;    % Break out of inner scan loop since not enough NET resources are available; any further tries would fail
               end
