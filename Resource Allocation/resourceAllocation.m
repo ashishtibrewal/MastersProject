b2ed2a04c9053ee47cc485e2d9c21dd1a29af00c
Ashish Tibrewal <at12060@my.bristol.ac.uk>
2016-06-22 13:34:27 +0100

    Minor bug fix.
     On branch networkAwareLocalityBased
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   Resource Allocation/resourceAllocation.m

--- Resource Allocation/resourceAllocation.m
+++ Resource Allocation/resourceAllocation.m
@@ -188,6 +188,8 @@ function [dataCenterMap, ITallocationResult, NETallocationResult, ITresourceNode
     ITfailureCause = 'NONE';        % Initialize/reset IT resource allocation failure cause for every iteration of the loop
     NETfailureCause = 'NONE';       % Initialize/reset NET resource allocation failure cause for every iteration of the loop
     pathLatenciesAllocated = {};    % Initialize/reset path latencies for every iteration of the loop
+    pathsUnitMax = {};              % Initialize/reset paths unit max for every iteration of the loop
+    pathsBandwidth = {};            % Initialize/reset paths bandwidth for every iteration of the loop
 
     % Store contention ratios
     CRs{iCR} = maxCRswitch;
